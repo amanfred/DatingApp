dc85f584a493d350fe9c129474dde98993a71f82
Ales Manfreda <alesmanfreda82@gmail.com>
2019-04-29 23:51:43 +0200

    Part 2

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { UserService } from './_services/user.service';
 import { appRoutes } from './routes';
 import { AuthService } from './_services/auth.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -33,7 +34,8 @@ export function tokenGetter() {
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MemberCardComponent
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
